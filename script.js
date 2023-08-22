c06bbd176d0032c2dfc1f2d560e69e0c81eab184
azabe2 <125875392+azabe2@users.noreply.github.com>
2023-08-22 17:11:20 +0100

    Added initial rules if player chooses rock

--- script.js
+++ script.js
@@ -9,8 +9,29 @@ const choice = ["rock", "paper", "scissors"];
 
 console.log(getComputerChoice(choice));
 // User is prompted to input their choice of rock, paper, or scissors
+const getPlayerSelection = prompt("Choose rock, paper, or scissors");
+
+console.log(getPlayerSelection)
 // Test is run to find a result
-// If user beats computer display win message
-// If user ties computer display tie message
-// If user loses to computer play losing message
+function playRound(playerSelection, computerSelection) {
+    // If user ties computer display tie message
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        return "It's a tie! You both chose Rock";
+    } 
+    // If user loses to computer play losing message
+    else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock";
+    }
+    // If user beats computer display win message 
+    else {
+        return "You win! Rock beats Scissors";
+    }
+    // If user ties computer display tie message
+  }
+   
+  const playerSelection = getPlayerSelection.toLowerCase;
+  const computerSelection = getComputerChoice(choice);
+
+  console.log(playRound(playerSelection, computerSelection));
+
 // Prompt user again until there is a score of 5 wins.
