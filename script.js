9b3d8770dff7757cc6c3b5335469a642811781ac
azabe2 <125875392+azabe2@users.noreply.github.com>
2023-08-22 15:49:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Computer has a random choice of rock, paper, or scissors
+// User is prompted to input their choice of rock, paper, or scissors
+// Test is run to find a result
+// If user beats computer display win message
+// If user ties computer display tie message
+// If user loses to computer play losing message
+// Prompt user again until there is a score of 5 wins.
