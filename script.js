ad7c83a4edada397426899aad93cc122a496010c
azabe2 <125875392+azabe2@users.noreply.github.com>
2023-08-23 12:33:06 +0100

    solved formatting issue resulting in wrong result

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-// Computer has a random choice of rock, paper, or scissors
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()*choice.length)];
-    }
+}
 
-// Test is run to find a result
 function playRound(playerSelection, computerSelection) {
-// If user ties computer display tie message
-    if (playerSelection === computerSelection) {
-        return `It's a tie! You both chose ${playerSelection}`;
-// If user beats computer display win message
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both chose ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats Scissors";
+    return "You win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats Rock";
+    return "You win! Paper beats Rock";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats Paper";
-// If user loses to computer play losing message
+    return "You win! Scissors beats Paper";
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    return `You lose ${computerSelection} beats ${playerSelection}`;
     }
   }
 
-// User is prompted to input their choice of rock, paper, or scissors
 const computerSelection = getComputerChoice();
-console.log(computerSelection)
+console.log(computerSelection);
 const playerSelection = prompt("Can you beat the machine!? Make a choice of either Rock, Paper, or Scissors").toLowerCase();
-console.log(playerSelection)
+console.log(playerSelection);
 console.log(playRound(playerSelection, computerSelection));
-
-// Prompt user again until there is a score of 5 wins.
