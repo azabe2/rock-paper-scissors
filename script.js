fe8c6e66d971afd09ee2011c6db79f3e23b5870e
azabe2 <125875392+azabe2@users.noreply.github.com>
2023-08-23 12:57:29 +0100

    Added player score variables

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `It's a tie! You both chose ${playerSelection}`;
+    return `It's a tie! You both chose ${playerSelection} ` + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats Scissors";
+    return "You win! Rock beats Scissors. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats Rock";
+    return "You win! Paper beats Rock. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats Paper";
+    return "You win! Scissors beats Paper. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
     } else {
-    return `You lose ${computerSelection} beats ${playerSelection}`;
+    return `You lose ${computerSelection} beats ${playerSelection}. ` + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
     }
   }
+
+let playerScore = 0;
+let computerScore = 0;
 
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
