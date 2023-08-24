cd92c26fc09de278b79be34aaa80ba97910aa6b4
azabe2 <125875392+azabe2@users.noreply.github.com>
2023-08-24 09:26:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    return `It's a tie! You both chose ${playerSelection} ` + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
+    return `It's a tie! You both chose ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats Scissors. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
+      playerScore++;
+      return "You win! Rock beats Scissors.";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats Rock. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
+      playerScore++;
+      return "You win! Paper beats Rock.";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats Paper. " + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
+      playerScore++;
+      return "You win! Scissors beats Paper.";
     } else {
-    return `You lose ${computerSelection} beats ${playerSelection}. ` + `Player Score: ${playerScore} ` + `Computer Score: ${computerScore}`;
+      computerScore++;
+      return `You lose ${computerSelection} beats ${playerSelection}.`;
     }
   }
 
 let playerScore = 0;
 let computerScore = 0;
 
+
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
 const playerSelection = prompt("Can you beat the machine!? Make a choice of either Rock, Paper, or Scissors").toLowerCase();
 console.log(playerSelection);
 console.log(playRound(playerSelection, computerSelection));
+console.log(`Player score: ${playerScore} Computer score: ${computerScore}`);
